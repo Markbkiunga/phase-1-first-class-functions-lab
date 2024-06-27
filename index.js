e496c1500b4138d163d0c85dc7a11bb36f95a6c6
Mark Brian <markbkiunga2017@gmail.com>
2024-06-27 18:23:38 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// let drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+createFareMultiplier = (multiplier) => (value) => value * multiplier;
+const fareDoubler = (fare) => fare * 2;
+const fareTripler = (fare) => fare * 3;
+const selectDifferentDrivers = (arrayOfDrivers, fn) => fn(arrayOfDrivers);
