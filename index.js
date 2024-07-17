1a2d0316afdb592251ff35fca1250ef8c72b9560
Mark Brian <markbkiunga2017@gmail.com>
2024-07-17 20:18:50 +0300

    Re-completes Lab

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // Code your solution in this file!
-// let drivers = ["Antonia", "Nuru", "Amari", "Mo"];
-const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
-const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+let drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = (array) => array.slice(0, 2);
+// console.log(returnFirstTwoDrivers(drivers));
+const returnLastTwoDrivers = (array) => array.slice(-2);
+// console.log(returnLastTwoDrivers(drivers));
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-createFareMultiplier = (multiplier) => (value) => value * multiplier;
-const fareDoubler = (fare) => fare * 2;
-const fareTripler = (fare) => fare * 3;
-const selectDifferentDrivers = (arrayOfDrivers, fn) => fn(arrayOfDrivers);
+function createFareMultiplier(multiplier) {
+  let multiplierFunction = (fare) => {
+    return fare * multiplier;
+  };
+  return multiplierFunction;
+}
+// console.log(createFareMultiplier(4));
+const fareDoubler = createFareMultiplier(2);
+// console.log(fareDoubler(5));
+
+const fareTripler = createFareMultiplier(3);
+// console.log(fareTripler(5));
+const selectDifferentDrivers = (array, fn) => {
+  //   console.log(fn(array));
+  return fn(array);
+};
+// selectDifferentDrivers(drivers, returnFirstTwoDrivers);
